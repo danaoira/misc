0f6bf5bc44c293c64547dec93078e403deee4dbb
Dana Oira <danaoira@live.com>
2016-08-07 03:56:10 -0700

    Fixed comments

--- Other/ClockworkWarsCleanup/run.py
+++ Other/ClockworkWarsCleanup/run.py
@@ -17,13 +17,13 @@ import random
 
 f = open('results.txt', 'w')
 alpha = ['C', 'F', 'L', 'M', 'S', 'T', 'V']
-num = 7	# max int value (see outline above)
+num = 7			# max int value (see outline above)
 
 def create_items(alpha):
 	items = []
 	for i in alpha:
-		for j in range(1, num + 1):	# range = 1-7
-			val = i + str(j)	# => 'C1', 'C2', etc
+		for j in range(1, num + 1):		# range(1, 8) = 1-7
+			val = i + str(j)			# val = 'C1', 'C2', etc
 			items.append(val)
 	random.shuffle(items)
 	return items
@@ -41,7 +41,7 @@ def write_result(val, is_matrix):
 def update_result(items):
 	for i in items:			# ex. i = C2
 		f.write('\n\nRound ' + str(items.index(i) + 1) + ' (' + str(i) + ')\n')
-		result[int(i[1]) - 1][alpha.index(i[0])] = i
+		result[int(i[1]) - 1][alpha.index(i[0])] = i			# result[1][0] = C2
 		write_result(result, True)
 
 print('Running Clockworks Wars Cleanup')
@@ -51,7 +51,7 @@ items = create_items(alpha)
 write_result(items, False)
 
 print('... created matrix')
-result = create_matrix(len(alpha), num)	# => create_matrix(7, 7)
+result = create_matrix(len(alpha), num)			# result = create_matrix(7, 7)
 
 print('... updated result')
 update_result(items)
