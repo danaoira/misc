588818cc2cdf1a4fbbc90f979b0398af8d7371f9
Dana Oira <danaoira@live.com>
2016-08-07 00:42:15 -0700

    Generate random list of tiles

--- Other/clockwork-wars-cleanup.py
+++ Other/clockwork-wars-cleanup.py
@@ -8,6 +8,23 @@
 # TO DO
 	# Value generation
 		# Values: V1-V7, C1-C7, L1-L7, F1-F7, T1-T7, M1-M7, S1-S7
+		# Regex: [(C|F|L|M|S|T|V)(1-7)]
 	# Randomize values
 	# Do some sort of sort
 	# Print each result
+
+import random
+
+def generate_tiles():
+	tiles = []
+	alpha = ['C', 'F', 'L', 'M', 'S', 'T', 'V']
+
+	for i in alpha:
+		for j in range(1, 8):
+			val = i + str(j)
+			tiles.append(val)
+	random.shuffle(tiles)
+	return tiles
+
+tiles = gen_tiles()
+print(tiles)
