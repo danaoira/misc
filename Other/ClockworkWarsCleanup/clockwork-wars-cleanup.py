b04b6668cd55b5fc97020af0ace3aafbd4b2be0d
Dana Oira <danaoira@live.com>
2016-08-07 02:40:00 -0700

    Minor updates to working first draft

--- Other/ClockworkWarsCleanup/clockwork-wars-cleanup.py
+++ Other/ClockworkWarsCleanup/clockwork-wars-cleanup.py
@@ -10,8 +10,8 @@
 		# Values: V1-V7, C1-C7, L1-L7, F1-F7, T1-T7, M1-M7, S1-S7
 		# RegEx: [(C|F|L|M|S|T|V)(1-7)]
 	# Randomize values
-	# Place 
-	# Print each result
+	# Place values into matrix
+	# Print results to file
 
 import random
 
@@ -24,11 +24,9 @@ def create_tiles(alpha):
 			val = i + str(j)
 			tiles.append(val)
 	random.shuffle(tiles)
-	print('... creating randomized list of tiles')
 	return tiles
 
 def create_matrix(row, col):
-	print('... creating matrix')
 	return [['  ' for x in range(col)] for y in range(row)]
 
 def write_input(f, val):
@@ -49,10 +47,15 @@ f = open('results.txt', 'w')
 
 print('Running Clockworks Wars Cleanup')
 
+print('... creating randomized list')
 tiles = create_tiles(alpha)
 write_input(f, tiles)
+
+print('... creating matrix')
 result = create_matrix(7, 7)
+
+print('... updating result')
 write_result(result)
 update_result(tiles)
 
-print('... completed sort of ' + str(len(tiles)) + ' tiles')
+print('... completed result for ' + str(len(tiles)) + ' tiles')
