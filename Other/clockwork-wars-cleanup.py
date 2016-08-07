d2941dc3ec797fbccb96315a7cdad1036138bb77
Dana Oira <danaoira@live.com>
2016-08-07 00:11:46 -0700

    Created requirements for Clockwork Wars Cleanup problem

---
+++ Other/clockwork-wars-cleanup.py
@@ -0,0 +1,13 @@
+# Project: 		Clockwork Wars Cleanup
+# Description: 	A program to simulate the tile cleanup for the Clockwork Wars board game.
+# Input: 		A randomized list of alphanumeric values.
+# Output:		A sorted list of alphanumeric values in a matrix.
+# Programmer:	Dana Oira Toribio
+# Duration:		8/6/2016 - TBA
+
+# TO DO
+	# Value generation
+		# Values: V1-V7, C1-C7, L1-L7, F1-F7, T1-T7, M1-M7, S1-S7
+	# Randomize values
+	# Do some sort of sort
+	# Print each result
