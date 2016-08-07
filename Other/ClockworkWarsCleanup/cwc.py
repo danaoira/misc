8107256538de1ba8aed4d979ca1e214984e4d3ee
Dana Oira <danaoira@live.com>
2016-08-07 03:34:39 -0700

    Automated matrix size

--- Other/ClockworkWarsCleanup/cwc.py
+++ Other/ClockworkWarsCleanup/cwc.py
@@ -1,9 +1,9 @@
-# Project: 		Clockwork Wars Cleanup
-# Description: 	A greedy sort to simulate the tile cleanup for the Clockwork Wars board game.
-# Input: 		A unsorted list of alphanumeric values.
+# Project:		Clockwork Wars Cleanup
+# Description:	A greedy sort to simulate the tile cleanup for the Clockwork Wars board game.
+# Input:		A randomized list of alphanumeric values.
 # Output:		A sorted list of alphanumeric values in a matrix.
-# Programmer:	Dana Oira Toribio
-# Duration:		8/6/2016 - 8/6/2016
+# Creator:		Dana Oira Toribio
+# Duration:		8/6/2016 - 8/7/2016
 
 # Outline:
 	# Value generation
@@ -17,11 +17,12 @@ import random
 
 f = open('results.txt', 'w')
 alpha = ['C', 'F', 'L', 'M', 'S', 'T', 'V']
+num = 7	# max int value (see outline above)
 
 def create_items(alpha):
 	items = []
 	for i in alpha:
-		for j in range(1, 8):
+		for j in range(1, num + 1):	# range = 1-7
 			val = i + str(j)	# => 'C1', 'C2', etc
 			items.append(val)
 	random.shuffle(items)
@@ -38,19 +39,19 @@ def write_result(val, is_matrix):
 			f.write('\n' + str(i))
 
 def update_result(items):
-	for i in items:
+	for i in items:			# ex. i = C2
 		f.write('\n\nRound ' + str(items.index(i) + 1) + ' (' + str(i) + ')\n')
-		result[int(i[1]) - 1][alpha.index(i[0])] = i	# ex. result[0][0] = C1
+		result[int(i[1]) - 1][alpha.index(i[0])] = i	# ex. result[1][0] = C2
 		write_result(result, True)
 
 print('Running Clockworks Wars Cleanup')
 
-print('... created unsorted list')
+print('... created randomized list')
 items = create_items(alpha)
 write_result(items, False)
 
 print('... created matrix')
-result = create_matrix(7, 7)
+result = create_matrix(len(alpha), num)	# => create_matrix(7, 7)
 
 print('... updated result')
 update_result(items)
