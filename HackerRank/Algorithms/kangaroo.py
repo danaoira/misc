af96d136a24a0df5cfb400cd7a6e1dc43fb997c6
Dana <danaoira@acm.org>
2016-10-18 00:31:08 -0700

    Completed kangaroo algorithm

---
+++ HackerRank/Algorithms/kangaroo.py
@@ -0,0 +1,41 @@
+#!/bin/python3
+
+import sys
+
+x1,v1,x2,v2 = input().strip().split(' ')
+x1,v1,x2,v2 = [int(x1),int(v1),int(x2),int(v2)]
+
+# update location
+def update_location(x, v):
+	x = x + v
+	return x
+
+# check if same location
+def is_same_location(a, b):
+	if a == b:
+		return True
+	else:
+		return False
+
+# check if distance decreases
+def is_dist_decr(x1, v1, x2, v2):
+	if (x1 > x2) and (v1 > v2):
+		return False
+	elif (x2 > x1) and (v2 > v1):
+		return False
+	else:
+		return True
+
+# set locations
+while(is_same_location(x1, x2) is False):
+	# error check distance decrease
+	if is_dist_decr(x1, v1, x2, v2) is False:
+		print('NO')
+		break
+
+	# update locations
+	x1 = update_location(x1, v1)
+	x2 = update_location(x2, v2)
+
+	if is_same_location(x1, x2) is True:
+		print('YES')
