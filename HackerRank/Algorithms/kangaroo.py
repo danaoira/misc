440c5e9099836cb09caa96b475ae2333132c7d63
Dana <danaoira@acm.org>
2017-02-06 00:21:40 -0800

    Update kangaroo.py

--- HackerRank/Algorithms/kangaroo.py
+++ HackerRank/Algorithms/kangaroo.py
@@ -5,6 +5,8 @@ import sys
 x1,v1,x2,v2 = input().strip().split(' ')
 x1,v1,x2,v2 = [int(x1),int(v1),int(x2),int(v2)]
 
+# check constraints
+
 # update location
 def update_location(x, v):
 	x = x + v
